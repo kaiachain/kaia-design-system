0a290e672694dbcb85a85856337a6c78cb757de9
blue <skqksh@gmail.com>
2024-07-11 12:16:41 +0900

    feat. Update example

---
+++ example/src/theme.ts
@@ -0,0 +1,23 @@
+import { darkTheme, lightTheme } from 'kaia-design-system'
+
+export const customDarkTheme = {
+  customColor: {
+    bg: 'red',
+    text: 'blue',
+  },
+  ...darkTheme,
+}
+
+export type ExThemeType = typeof customDarkTheme
+
+export const customLightTheme: ExThemeType = {
+  customColor: {
+    bg: 'yellow',
+    text: 'green',
+  },
+  ...lightTheme,
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ExThemeType {}
+}
