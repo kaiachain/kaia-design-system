56893d39116cea196d840dcf292752c8a89c3ea4
blue <skqksh@gmail.com>
2024-10-18 15:35:55 +0900

    fix. lower/upper case error of file I -> i

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,21 +1,21 @@
-import ArrowLineUpRight from './ArrowLineUpRight.svg';
-import ArrowUpRight from './ArrowUpRight.svg';
-import Check from './Check.svg';
-import ChevronBottom from './ChevronBottom.svg';
-import ChevronLeft from './ChevronLeft.svg';
-import ChevronRight from './ChevronRight.svg';
-import ChevronTop from './ChevronTop.svg';
-import CircleInfoFilled from './CircleInfoFilled.svg';
-import CircleQuestion from './CircleQuestion.svg';
-import IndentDownRight from './IndentDownRight.svg';
-import Minus from './Minus.svg';
-import Paste from './Paste.svg';
-import Plus from './Plus.svg';
-import Search from './Search.svg';
-import X from './X.svg';
+import ArrowBarUpRight from './ArrowBarUpRight.svg'
+import ArrowUpRight from './ArrowUpRight.svg'
+import Check from './Check.svg'
+import ChevronBottom from './ChevronBottom.svg'
+import ChevronLeft from './ChevronLeft.svg'
+import ChevronRight from './ChevronRight.svg'
+import ChevronTop from './ChevronTop.svg'
+import CircleInfoFilled from './CircleInfoFilled.svg'
+import CircleQuestion from './CircleQuestion.svg'
+import IndentDownRight from './IndentDownRight.svg'
+import Minus from './Minus.svg'
+import Paste from './Paste.svg'
+import Plus from './Plus.svg'
+import Search from './Search.svg'
+import X from './X.svg'
 
 export default {
-  ArrowLineUpRight,
+  ArrowBarUpRight,
   ArrowUpRight,
   Check,
   ChevronBottom,
@@ -30,4 +30,4 @@ export default {
   Plus,
   Search,
   X,
-};
+}
